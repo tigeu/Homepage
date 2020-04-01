ec2e360a6fbdecb400b240d4cb4106c1c4403393
Sebastien Croize-Pourcelet <sebastien.croize-pourcelet@hhu.de>
2020-04-01 13:00:54 +0200

    Add import of logging dependencies to controller classes for demonstration of logging functionality

--- website/src/main/java/de/hhu/website/controller/HomeController.java
+++ website/src/main/java/de/hhu/website/controller/HomeController.java
@@ -1,11 +1,14 @@
 package de.hhu.website.controller;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.security.access.annotation.Secured;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
 public class HomeController {
+  final static Logger logger = LoggerFactory.getLogger(HomeController.class);
 
   @GetMapping("/")
   @Secured("ROLE_user")
