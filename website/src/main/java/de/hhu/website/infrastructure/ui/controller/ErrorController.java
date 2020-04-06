602a50c39e229a7dc4f2bdda5cc3ead17c17275b
Sebastien Croize-Pourcelet <sebastien.croize-pourcelet@hhu.de>
2020-04-06 19:09:03 +0200

    Declare parameter and local variables final as suggested by pmd.

--- website/src/main/java/de/hhu/website/infrastructure/ui/controller/ErrorController.java
+++ website/src/main/java/de/hhu/website/infrastructure/ui/controller/ErrorController.java
@@ -28,11 +28,11 @@ public class ErrorController implements org.springframework.boot.web.servlet.err
    * @return this returns the appropriate html-view.
    */
   @RequestMapping("/error")
-  public String handleError(HttpServletRequest request) {
-    Object status = request.getAttribute(RequestDispatcher.ERROR_STATUS_CODE);
+  public String handleError(final HttpServletRequest request) {
+    final Object status = request.getAttribute(RequestDispatcher.ERROR_STATUS_CODE);
 
     if (status != null) {
-      int statusCode = Integer.parseInt(status.toString());
+      final int statusCode = Integer.parseInt(status.toString());
 
       if (statusCode == HttpStatus.NOT_FOUND.value()) {
         return "error-404";
