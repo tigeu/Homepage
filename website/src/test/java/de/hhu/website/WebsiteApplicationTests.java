e3cf3af81f1e80f748f90534069f26104d05ee17
Sebastien Croize-Pourcelet <sebastien.croize-pourcelet@hhu.de>
2020-04-06 18:05:54 +0200

    Remove obsolete SuppressWarnings and comments

--- website/src/test/java/de/hhu/website/WebsiteApplicationTests.java
+++ website/src/test/java/de/hhu/website/WebsiteApplicationTests.java
@@ -7,10 +7,7 @@ import org.springframework.boot.test.context.SpringBootTest;
 class WebsiteApplicationTests {
 
   @Test
-  // This test is Spring standard and works correctly without assert
-  // Method contextLoads must not be package private
-  @SuppressWarnings({"PMD.JUnitTestsShouldIncludeAssert", "PMD.DefaultPackage"})
-    /* default */ void contextLoads() {
+  void contextLoads() {
   }
 
 }
